60847ffc8a39e8f9d0dc237e535800b8e3024452
Rayhan Zou <rayhan.zou98@gmail.com>
2025-10-30 20:13:42 +0700

    fix: update api index for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
+import { VersioningType } from '@nestjs/common';
 
 let server: any;
 
 export default async function handler(req: any, res: any) {
   if (!server) {
     const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'log'] });
+    
+    // Add global prefix
+    app.setGlobalPrefix('api');
+    
+    // Add versioning
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: '1'
+    });
+    
     await app.init();
     server = app.getHttpAdapter().getInstance();
   }
